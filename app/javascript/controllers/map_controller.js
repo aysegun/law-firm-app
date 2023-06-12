411758a2f9a222b9ed94822c313a187b4aafd9ce
ayse gungormez <aysegngrmz.91@windowslive.com>
2023-06-12 17:21:51 +0300

    google maps integrated

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,46 +2,66 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="map"
 export default class extends Controller {
-  static values = {
-    apiKey: String,
-    latitude: Number,
-    longitude: Number,
-    markers: Array
-  }
+  // static values = {
+  //   apiKey: String,
+  //   latitude: Number,
+  //   longitude: Number,
+  //   markers: Array
+  // }
+  // connect() {
+  //   mapboxgl.accessToken = this.apiKeyValue
+  //   console.log("hello")
+  //   this.map = new mapboxgl.Map({
+  //     container: this.element,
+  //     style: "mapbox://styles/mapbox/streets-v10",
+  //     //center: [41.3987283, 2.16344],
+  //     dragPan: true,
+  //     cooperativeGestures: true,
+  //   })
+
+  //   this.#addMarkersToMap()
+  //   this.#fitMapToMarkers()
+
+  //   this.map.scrollZoom.disable();
+  //   this.map.addControl(new mapboxgl.NavigationControl());
+  // }
+
+  // #addMarkersToMap() {
+  //   this.markersValue.forEach((marker) => {
+  //     const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+  //     const customMarker = document.createElement("div")
+  //     customMarker.innerHTML = marker.marker_html
+
+  //     new mapboxgl.Marker(customMarker)
+  //       .setLngLat([ marker.lng, marker.lat ])
+  //       .setPopup(popup)
+  //       .addTo(this.map)
+  //   })
+  // }
+
+  // #fitMapToMarkers() {
+  //   const bounds = new mapboxgl.LngLatBounds()
+  //   this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+  //   this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  // }
+  static targets = ["map", "latitude", "longitude"]
   connect() {
-    mapboxgl.accessToken = this.apiKeyValue
-    console.log("hello")
-    this.map = new mapboxgl.Map({
-      container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10",
-      //center: [41.3987283, 2.16344],
-      dragPan: true,
-      cooperativeGestures: true,
-    })
-
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
-
-    this.map.scrollZoom.disable();
-    this.map.addControl(new mapboxgl.NavigationControl());
+    if (typeof (google) !="undefined") {
+      this.initializeMap()
+    }
   }
 
-  #addMarkersToMap() {
-    this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
-      const customMarker = document.createElement("div")
-      customMarker.innerHTML = marker.marker_html
-
-      new mapboxgl.Marker(customMarker)
-        .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup)
-        .addTo(this.map)
-    })
+  initializeMap(){
+    this.map()
+    this.marker()
   }
 
-  #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  map() {
+    if(this._map == undefined) {
+      this._map = new google.maps.Map(this.mapTarget, {
+        zoom: 17
+      })
+    }
+    return this._map
   }
 }
